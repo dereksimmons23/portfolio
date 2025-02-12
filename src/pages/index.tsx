02320097fbecfdde50166e16506bf8c6aa546f22
Derek Simmons <simmons.derek@gmail.com>
2025-02-12 11:07:43 -0600

    Initial portfolio site setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PortfolioSite from '../components/PortfolioSite';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
+      <PortfolioSite />
+    </main>
+  );
+}
